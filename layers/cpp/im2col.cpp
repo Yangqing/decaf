eb1f22307d117a3b82aadcdd28701672feb88640
Yangqing Jia <jiayq84@gmail.com>
2013-07-31 12:54:30 -0700

    deleted the said unused im2col_per_row code.

--- layers/cpp/im2col.cpp
+++ layers/cpp/im2col.cpp
@@ -40,40 +40,6 @@ inline void im2col(const Dtype* data_im,
     }
 } // im2col
 
-/* Not used. Does not provide speedup. Will delete in the future.
-template <typename Dtype>
-inline void im2col_per_row(const Dtype* data_im,
-        const int height,
-        const int width,
-        const int nchannels,
-        const int psize,
-        const int stride,
-        Dtype* data_col) {
-    // The naive im2col implementation
-    int step_im = width * nchannels;
-    int step_col = psize * nchannels;
-    int height_col = (height - psize) / stride + 1;
-    int width_col = (width - psize) / stride + 1;
-#pragma omp parallel for
-    for (int idxh = 0; idxh < height_col; ++idxh) {
-        // here what we do is to have a per-row copy.
-        for (int row = 0; row < psize; ++row) {
-            Dtype* pointer_col = data_col + 
-                idxh * width_col * psize * step_col + row * step_col;
-            const Dtype* pointer_im = data_im + 
-                (idxh * stride + row) * step_im;
-            for (int idxw = 0; idxw < width_col; ++idxw) {
-                // copy image[i, idxw:idxw+psize, :]
-                for (int j = 0; j < step_col; ++j) {
-                    pointer_col[j] = pointer_im[j];
-                }
-                pointer_col += psize * step_col;
-                pointer_im += stride * nchannels;
-            }
-        }
-    }
-} // im2col_per_row
-*/
 
 template <typename Dtype>
 inline void col2im(Dtype* data_im,
