83bc1626b89ed77408f682164263879973b89e43
Yangqing Jia <jiayq84@gmail.com>
2013-08-29 11:21:04 -0700

    a barely working setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+# This is Yangqing's extremely hacky code to install decaf on your machine
+# assuming that you are using a Unix-like system. To install, do:
+#   python setup.py build
+#   python setup.py install
+# You should be able to then use decaf in your python environment.
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 from distutils.util import convert_path
 from fnmatch import fnmatchcase
 import os
@@ -21,14 +26,23 @@ def find_packages(where='.'):
         out = [item for item in out if not fnmatchcase(item, pat)]
     return out
 
+def run_setup():
+    # Before running setup, let's do a make since we have C shared libraries
+    # to be build (note that they are not python extensions...)
+    # It's kind of ugly, but let's just keep it this way for simplicity
+    os.system('cd decaf; make; cd ..')
+    setup(name='decaf',
+          version='0.9',
+          description='Deep convolutional neural network framework',
+          author='Yangqing Jia',
+          author_email='jiayq84@gmail.com',
+          packages=find_packages(),
+          package_data={'decaf': ['util/_data/*'],
+                        'decaf.demos.jeffnet': ['static/*', 'templates/*'],
+                        'decaf.demos.notebooks': ['*.ipynb'],
+                        'decaf.layers.cpp': ['libcpputil.so'],
+                       },
+         )
 
-setup(name='decaf',
-      version='0.9',
-      description='Deep convolutional neural network framework',
-      author='Yangqing Jia',
-      author_email='jiayq84@gmail.com',
-      packages=find_packages(),
-      package_data={'decaf': ['util/_data/*'],
-                    'decaf.demos.jeffnet': ['static/*', 'templates/*'],
-                    'decaf.demos.notebooks': ['*.ipynb']},
-     )
+if __name__ == '__main__':
+    run_setup()
