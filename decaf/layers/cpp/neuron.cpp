6e171c40f32f425c4f2e5138a2755a4cf170780d
Yangqing Jia <jiayq84@gmail.com>
2013-09-10 17:27:45 -0700

    neuron cpp fix

--- decaf/layers/cpp/neuron.cpp
+++ decaf/layers/cpp/neuron.cpp
@@ -1,7 +1,4 @@
 #include <algorithm>
-#ifdef DECAF_USE_MKL
-#include <mkl_vml.h>
-#endif // DECAF_USE_MKL
 #include "neuron.h"
 
 using std::max;
@@ -17,21 +14,6 @@ inline void _relu_forward(const Dtype* input, Dtype* output, int n) {
 extern "C" {
 
 void relu_forward(const int len, const void* input, void* output, int n) {
-#ifdef DECAF_USE_MKL
-    switch(len) {
-    case sizeof(float):
-        vsAbs(n, (const float*)input, (float*)output);
-        vsAdd(n, (const float*)input, (const float*)output, (float*)output);
-        break;
-    case sizeof(double):
-        vdAbs(n, (const double*)input, (double*)output);
-        vdAdd(n, (const double*)input, (const double*)output, (double*)output);
-        break;
-    default:
-        exit(EXIT_FAILURE);
-    } // switch(len)
-
-#else
     switch(len) {
     case sizeof(float):
         _relu_forward<float>((const float*) input, (float*) output, n);
@@ -42,7 +24,6 @@ void relu_forward(const int len, const void* input, void* output, int n) {
     default:
         exit(EXIT_FAILURE);
     } // switch(len)
-#endif // DECAF_USE_MKL
 }
 
 }
