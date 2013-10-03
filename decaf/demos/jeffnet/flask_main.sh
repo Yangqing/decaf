65d7c3e01a1c6112f601d1049651e25d4203008b
Yangqing Jia <jiayq84@gmail.com>
2013-10-03 13:37:03 -0700

    flask script

--- decaf/demos/jeffnet/flask_main.sh
+++ decaf/demos/jeffnet/flask_main.sh
@@ -5,6 +5,8 @@
 #SBATCH --mem=8000
 #SBATCH --nodelist=orange5
 
+export MKL_NUM_THREADS=24
+export OMP_NUM_THREADS=24
 python flask_main.py \
     --net_file=../../scripts/imagenet.jeffnet.epoch72 \
     --meta_file=../../scripts/imagenet.jeffnet.meta
